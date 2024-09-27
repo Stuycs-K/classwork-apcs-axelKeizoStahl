f58dfb6d483ad2ec7acfa84fb6393b2913dde5fe
axelKeizoStahl <axelkeizo@gmail.com>
2024-09-27 10:15:29 -0400

    copyArr

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -19,4 +19,12 @@ public class ArrayWrk {
         }
         return res + "]";
     }
+
+    public static int[] returnCopy(int[] arr) {
+        int[] copy = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+        return copy;
+    }
 }
