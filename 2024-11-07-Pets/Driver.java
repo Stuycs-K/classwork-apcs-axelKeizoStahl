40f36b93eafbbdc2ed35e51dfc739fa686dce4fd
axelKeizoStahl <axelkeizo@gmail.com>
2024-11-07 09:59:21 -0500

    Bird constructor and speak impl

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -4,5 +4,8 @@ public class Driver {
     public static void main(String[] args) {
         Animal a1 = new Animal("ying yang", 7, "AZ");
         a1.speak();
+
+        Bird b1 = new Bird("matcha", 18, "AS", 6.3, "green");
+        b1.speak();
     }
 }
