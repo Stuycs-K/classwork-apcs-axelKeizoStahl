0edf77393cd9ff7942fd4c3dcfdf8e8d282f62ff
axelKeizoStahl <axelkeizo@gmail.com>
2024-11-07 10:15:19 -0500

    added step nine

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -3,6 +3,10 @@
 // Amy Zhou
 public class Driver {
     public static void main(String[] args) {
+        Animal a1 = new Animal(...);
+        Bird b1 = new Bird(...);
+        Bird b2 = new Animal(...);
+        Animal a2 = new Bird(...);
         Animal a1 = new Animal("ying yang", 7, "AZ");
         a1.speak();
 
