0638e046662189efccc7284f03dc22c205920b78
Axel Stahl <axelkeizo@proton.me>
2024-11-19 20:15:54 -0500

    day 1 aoc phase 2 impl

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -47,11 +47,9 @@ public class Day1 {
             int dirIndex = 0;
             String line = scanner.nextLine();
             String[] input = line.split(", ");
+	    positions.add(new int[]{0, 0});
             for (int i = 0; i < input.length; i++) {
-                
-                System.out.println(Arrays.toString(posxy));
-                positions.add(posxy);
-                System.out.println(Arrays.toString(positions.get(positions.size()-1)));
+		System.out.println(Arrays.toString(positions.get(positions.size()-1)));
                 if (input[i].charAt(0) == 'R') {
                     dirIndex = (dirIndex + 1)%4;
                 } else {
@@ -60,14 +58,18 @@ public class Day1 {
 
                 int blocks = Integer.parseInt(input[i].substring(1));
 
-                posxy[0] += directions[dirIndex][0] * blocks;
-                posxy[1] += directions[dirIndex][1] * blocks;
+		for (int k = 0; k < blocks; k++) {
+			posxy[0] += directions[dirIndex][0];
+			posxy[1] += directions[dirIndex][1];
+			for (int j = 0; j < positions.size(); j++) {
+			    if (positions.get(j)[0] == posxy[0] && positions.get(j)[1] == posxy[1]) {
+				return Math.abs(positions.get(j)[0]) + Math.abs(positions.get(j)[1]);
+			    }
+			}
+			positions.add(new int[]{posxy[0], posxy[1]});
+	    	}
+
                 System.out.println(Arrays.toString(posxy));
-                for (int j = 0; j < positions.size(); j++) {
-                    if (positions.get(j)[0] == posxy[0] && positions.get(j)[1] == posxy[1]) {
-                        return Math.abs(positions.get(j)[0]) + Math.abs(positions.get(j)[1]);
-                    }
-                }
             }
             scanner.close();
 	    return Math.abs(posxy[0]) + Math.abs(posxy[1]);
