cd8e06aad73b7a0183ae82678b8d5fe15a94cb94
axelKeizoStahl <axelkeizo@gmail.com>
2024-12-06 09:58:26 -0500

    test attack

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
         Adventurer p1 = new Warrior("Bob",10);
+        Adventurer p2 = new Warrior("Joe",10);
+        p1.attack(p2);
     }
 }
