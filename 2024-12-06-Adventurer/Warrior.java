3f49efe770bb5b340c1e065e7c3798daa7285db7
axelKeizoStahl <axelkeizo@gmail.com>
2024-12-06 09:57:37 -0500

    working syntax and constructors

---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,45 @@
+public class Warrior extends Adventurer {
+    private int special,maxSpecial;
+    public Warrior(String name) {
+        super(name);
+        special = 5;
+        maxSpecial = 5;
+    }
+    public Warrior(String name, int hp) {
+        this(name, hp, 5);
+    }
+    public Warrior(String name, int hp, int special) {
+        super(name, hp);
+        this.special = special;
+        maxSpecial = special;
+    }
+        
+    public String getSpecialName() {
+        return "PowerSlash";
+    }
+    public int getSpecial() {
+        return special;
+    }
+    public void setSpecial(int n) {
+        special = n;
+    }
+    public int getSpecialMax() {
+        return maxSpecial;
+    }
+
+    public String attack(Adventurer other) {
+        other.applyDamage(1);
+        return "ha";
+    }
+    public String support(Adventurer other) {
+        other.setHP(other.getHP()+1);
+        return "much better!";
+    }
+    public String support() {
+        return support(this);
+    }
+    public String specialAttack(Adventurer other) {
+        restoreSpecial(-1);
+        return attack(other) + ", " + getSpecialName();
+    }
+}
