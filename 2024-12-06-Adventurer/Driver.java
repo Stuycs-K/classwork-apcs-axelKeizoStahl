fa501ab93a60bf65676cbe9d16dfc6f1ef4513a9
Axel Stahl <axelkeizo@proton.me>
2024-12-08 17:10:29 -0500

    more descriptive Warrior diaouge

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,14 +1,14 @@
 public class Driver {
     public static void main(String[] args) {
         Adventurer w1 = new Warrior("Thor", 25, 12);
-        System.out.println(w1.getName() + " is a Warrior with " + w1.getName() + " specials left.");
+        System.out.println(w1.getName() + " is a Warrior with " + w1.getSpecialName() + ".");
         Adventurer w2 = new Warrior("Loki", 25, 12);
         
         System.out.println(w2.attack(w1));
         System.out.println(w2.attack(w1));
         
         System.out.println(w1.support(w2));
-        System.out.println(w1.getName() + " is a Warrior with " + w1.getName() + " specials left.");
+        System.out.println(w1.getName() + " is a Warrior with " + w1.getSpecial() + " specials left.");
     }
 }
 
--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,45 +1,54 @@
 public class Warrior extends Adventurer {
-    private int special,maxSpecial;
+    private int special, maxSpecial;
+    
     public Warrior(String name) {
         super(name);
         special = 5;
         maxSpecial = 5;
     }
+
     public Warrior(String name, int hp) {
         this(name, hp, 5);
     }
+
     public Warrior(String name, int hp, int special) {
         super(name, hp);
         this.special = special;
         maxSpecial = special;
     }
-        
+
     public String getSpecialName() {
         return "PowerSlash";
     }
+
     public int getSpecial() {
         return special;
     }
+
     public void setSpecial(int n) {
         special = n;
     }
+
     public int getSpecialMax() {
         return maxSpecial;
     }
 
     public String attack(Adventurer other) {
         other.applyDamage(1);
-        return "ha";
+        return getName() + " attacks " + other.getName() + ", boom!";
     }
+
     public String support(Adventurer other) {
-        other.setHP(other.getHP()+1);
-        return "much better!";
+        other.setHP(other.getHP() + 1);
+        return getName() + " supports " + other.getName() + ", much better!";
     }
+
     public String support() {
         return support(this);
     }
+
     public String specialAttack(Adventurer other) {
         restoreSpecial(-1);
-        return attack(other) + ", " + getSpecialName();
+        return getName() + " uses " + getSpecialName() + " on " + other.getName() + ", " + attack(other);
     }
 }
