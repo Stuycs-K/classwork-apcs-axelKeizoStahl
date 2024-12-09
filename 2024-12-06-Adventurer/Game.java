17245e01c0f9a0c200c96ac7485da7673d1da082
axelKeizoStahl <axelkeizo@gmail.com>
2024-12-09 10:13:22 -0500

    print stats

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 class Game {
+    public static void printStats(Adventurer adv) {
+        System.out.println(adv.getName() + " stats:");
+        System.out.println("HP: " + adv.getHP() + "/" + adv.getMaxHP());
+        System.out.println(adv.getSpecialName() + " capacity: " + adv.special() + "/" + adv.getSpecialMax());
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
