a64072b24a01be9a27ab792e7bca53d80ba58bd0
Axel Stahl <axelkeizo@proton.me>
2024-12-09 17:06:00 -0500

    Game syntax errors

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 class Game {
     public static void printStats(Adventurer adv) {
         System.out.println(adv.getName() + " stats:");
-        System.out.println("HP: " + adv.getHP() + "/" + adv.getMaxHP());
-        System.out.println(adv.getSpecialName() + " capacity: " + adv.special() + "/" + adv.getSpecialMax());
+        System.out.println("HP: " + adv.getHP() + "/" + adv.getmaxHP());
+        System.out.println(adv.getSpecialName() + " capacity: " + adv.getSpecial() + "/" + adv.getSpecialMax());
     }
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
