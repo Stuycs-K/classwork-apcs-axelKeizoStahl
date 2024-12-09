14fa2eb1f5d4394245d160df48cd267c9b59c20c
axelKeizoStahl <axelkeizo@gmail.com>
2024-12-09 10:14:29 -0500

    Game.getStats syntax

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,10 +4,13 @@ class Game {
         System.out.println(adv.getName() + " stats:");
         System.out.println("HP: " + adv.getHP() + "/" + adv.getMaxHP());
         System.out.println(adv.getSpecialName() + " capacity: " + adv.special() + "/" + adv.getSpecialMax());
+    }
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
         Adventurer comp = new CodeWarrior("Computer", 10, "perl");
         Adventurer player = new Warrior("Bob", 10, 10);
+        printStats(comp);
+        printStats(player);
     }
 }
