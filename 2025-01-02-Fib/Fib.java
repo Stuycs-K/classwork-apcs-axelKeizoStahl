9862ccec2b60bc3a755fd913ec7a2709ff447f27
axelKeizoStahl <axelkeizo@gmail.com>
2025-01-02 09:57:25 -0500

    Fib code

---
+++ 2025-01-02-Fib/Fib.java
@@ -0,0 +1,12 @@
+public class Fib {
+    public static void main(String[] args) {
+        System.out.println("fib(10): " + fib(10));
+    }
+    public static int fib(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
