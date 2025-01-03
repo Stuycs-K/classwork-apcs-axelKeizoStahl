6e8d27e474c1d1ff7162215bfe501ed0ebf4225b
axelKeizoStahl <axelkeizo@gmail.com>
2025-01-03 10:03:46 -0500

    makeWords

--- 2025-01-02-Fib/Fib.java
+++ 2025-01-02-Fib/Fib.java
@@ -1,6 +1,16 @@
 public class Fib {
     public static void main(String[] args) {
-        System.out.println("fib(10): " + fib(10));
+        System.out.println("makeWords(2,\"\",\"abc\"): ");
+         makeWords(2,"","abc");
+    }
+    public static void makeWords(int left, String res, String alphabet) {
+        if (left == 0) {
+            System.out.println(res);
+        } else {
+            for (char c: alphabet.toCharArray()) {
+                makeWords(left-1, res + c, alphabet);
+            }
+        }
     }
     public static int fib(int n) {
         if (n < 2) {
